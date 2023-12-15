822e2e5bfc0edc61ff444e640f2b5007e6633de6
DharunThota <125078188+DharunThota@users.noreply.github.com>
2023-12-15 18:01:23 +0530

    Create p4.cpp

---
+++ c/p4.cpp
@@ -0,0 +1,27 @@
+//program to emulate the caesar cipher, a simple encryption technique that involves shifting the letters of the alphabet, k positions to the right (or left).
+// eg: s = "abc", k = 2, direction = "encode" => "cde"
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string caesar(string s, int k, string direction){
+    string ans = "";
+    for(int i=0;i<s.length();i++){
+        if(direction == "encode"){
+            ans += s[i] + k;
+        }
+        else if(direction == "decode"){
+            ans += s[i] - k;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    string s, direction;
+    int k;
+    cin >> s >> k >> direction;
+    cout<<caesar(s,k,direction)<<endl;
+    return 0;
+}
