cfb2a51c34663cbc4c5c7720fec157714853ad1d
DharunThota <125078188+DharunThota@users.noreply.github.com>
2024-01-03 13:31:09 +0530

    Create p4.py

---
+++ py/p4.py
@@ -0,0 +1,18 @@
+# Python program for counting sort
+
+def countSort(arr):
+    k = max(arr)
+    count = [0 for i in range(k+1)]
+    ans = [0 for i in range(len(arr))]
+    for i in arr:
+        count[i] += 1
+    for i in range(k+1):
+        count[i] += count[i-1]
+    for i in range(len(arr)-1, -1, -1):
+        ans[count[arr[i]]] = arr[i]
+        count[arr[i]] -= 1
+    return ans 
+
+arr = [3, 4, 12, 2, 4, 5, 5, 6, 12, 33, 10, 1, 2, 8, 6]
+ans = countSort(arr)
+print (f"Sorted character array is {ans}")
