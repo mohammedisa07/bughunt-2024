b952e156ed71aaea78e63def94f763a11c0f50a0
DharunThota <125078188+DharunThota@users.noreply.github.com>
2024-01-08 23:02:41 +0530

    Create p6.py

---
+++ py/p6.py
@@ -0,0 +1,21 @@
+#program to perform binary addition
+
+def addBinary(a: str, b: str) -> str:
+    s = []
+    carry = 0
+    i = len(a) - 1
+    j = len(b) - 1
+
+    while i >= 0 or j >= 0:
+      if i >= 0:
+        carry += int(a[i])
+        i -= 1
+      if j >= 0:
+        carry += int(b[j])
+        j -= 1
+      s.append(str(carry % 2))
+      carry //= 2
+
+    return ''.join(reversed(s))
+
+print(addBinary("11", "0"))
