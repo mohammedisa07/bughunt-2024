ff4a115a3df23ba49898231f01e38dfd1c3d73e9
Jyolsna J Isac <133155647+jyolx@users.noreply.github.com>
2024-01-17 23:36:36 +0530

    Create p11.py

---
+++ py/p11.py
@@ -0,0 +1,10 @@
+# Function to convert decimal to binary number
+
+def binaryconversion(n):
+   print(n % 2,end = '')
+   if n > 1:
+       binaryconversion(n/2)
+
+number=int(input("Enter Number: "))
+binaryconversion(number)
+print()
