22e302d5b3eae83f12a455905e29aa1f04b245e6
MOHAMMED ISA S A <116036872+mohammedisa07@users.noreply.github.com>
2024-03-16 10:16:04 +0530

    Update p6.py

--- py/p6.py
+++ py/p6.py
@@ -6,16 +6,17 @@ def addBinary(a: str, b: str) -> str:
     i = len(a) - 1
     j = len(b) - 1
 
-    while i >= 0 or j >= 0:
-      if i >= 0:
-        carry += int(a[i])
-        i -= 1
-      if j >= 0:
-        carry += int(b[j])
-        j -= 1
-      s.append(str(carry % 2))
-      carry //= 2
+    while i >= 0 or j >= 0 or carry:
+        if i >= 0:
+            carry += int(a[i])
+            i -= 1
+        if j >= 0:
+            carry += int(b[j])
+            j -= 1
+        s.append(str(carry % 2))
+        carry //= 2
 
     return ''.join(reversed(s))
 
 print(addBinary("11", "0"))
+
