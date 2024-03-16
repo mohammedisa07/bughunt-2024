0dd230ba0f72a0c09cc35d3cf64bedca12750212
MOHAMMED ISA S A <116036872+mohammedisa07@users.noreply.github.com>
2024-03-16 09:56:22 +0530

    Update p3.py

--- py/p3.py
+++ py/p3.py
@@ -1,13 +1,9 @@
 #program to remove all even numbers from the beginning of a list
-#eg: [2, 4, 6, 17, 10] -> [17, 10] 
+def delete_starting_evens(lst):
+    while lst and lst[0] % 2 == 0:
+        lst.pop(0)
+    return lst
 
-def delete_starting_evens(list):
-  for item in list:
-    if list[0] % 2 == 0:
-      list.pop(0)
-    else:
-      break
-  return list
+lst = [2, 8, 10, 11]
+print(delete_starting_evens(lst))
 
-list = [2, 8, 10, 11]
-print(delete_starting_evens(list))
