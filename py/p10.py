4dbc4fa9bff12960a7d06f2ca3df8c0f8a8ddf09
MOHAMMED ISA S A <116036872+mohammedisa07@users.noreply.github.com>
2024-03-16 09:34:28 +0530

    Update p10.py

--- py/p10.py
+++ py/p10.py
@@ -1,16 +1,16 @@
-#Finding Average RGB values using the listmaker fuction to generate list of rgb values for a pixel
-
-def listmaker(r,g,b,list1=[]):
+def listmaker(r, g, b, list1=None):
+    if list1 is None:
+        list1 = []
     list1.append(r)
     list1.append(g)
     list1.append(b)
     return list1
 
-pixel1=listmaker(23,78,34)
-pixel2=listmaker(210,56,67)
-pixel3=listmaker(23,78,248)
-r=(pixel1[0]+pixel2[0]+pixel3[0])/3
-g=(pixel1[1]+pixel2[1]+pixel3[1])/3
-b=(pixel1[2]+pixel2[2]+pixel3[2])/3
-rgb=[r,g,b]
-print("Average RGB values of the pixels are",rgb)
+pixel1 = listmaker(23, 78, 34)
+pixel2 = listmaker(210, 56, 67)
+pixel3 = listmaker(23, 78, 248)
+r = (pixel1[0] + pixel2[0] + pixel3[0]) / 3
+g = (pixel1[1] + pixel2[1] + pixel3[1]) / 3
+b = (pixel1[2] + pixel2[2] + pixel3[2]) / 3
+rgb = [r, g, b]
+print("Average RGB values of the pixels are", rgb)
