57a03b8c440753128858bad429fa2a993f89a1a4
MOHAMMED ISA S A <116036872+mohammedisa07@users.noreply.github.com>
2024-03-16 10:22:13 +0530

    Update p7.py

--- py/p7.py
+++ py/p7.py
@@ -2,7 +2,7 @@
 #e.g. 2->4->3 + 5->6->4 = 7->0->8 (342 + 465 = 807)
 
 # Definition for singly-linked list.
-class ListNode(object):
+class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
@@ -14,21 +14,18 @@ class Solution:
         tail = dummyHead
         carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
-            digit1 = l1.val if l1 is not None else 0
-            digit2 = l2.val if l2 is not None else 0
+        while l1 or l2 or carry:
+            digit1 = l1.val if l1 else 0
+            digit2 = l2.val if l2 else 0
 
-            sum = digit1 + digit2 + carry
-            digit = sum % 10
-            carry = sum // 10
+            total = digit1 + digit2 + carry
+            digit = total % 10
+            carry = total // 10
 
-            newNode = ListNode(digit)
-            tail.next = newNode
+            tail.next = ListNode(digit)
             tail = tail.next
 
-            l1 = l1.next if l1 is not None else None
-            l2 = l2.next if l2 is not None else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        result = dummyHead.next
-        dummyHead.next = None
-        return result
+        return dummyHead.next
