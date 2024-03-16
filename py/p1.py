22ffe25e2396b5656e06d657bb3499f2bd742e65
MOHAMMED ISA S A <116036872+mohammedisa07@users.noreply.github.com>
2024-03-16 09:44:11 +0530

    Update p1.py

--- py/p1.py
+++ py/p1.py
@@ -1,16 +1,13 @@
-#program to print a tree pattern with asterisks
-# eg :- height = 3
-#output :-   *
-#           ***
-#          *****
+def asterisk_tree(height, level):
+    if level > height:
+        return
+    for _ in range(height - level):
+        print(" ", end="")
+    for _ in range(2 * level - 1):
+        print("*", end="")
+    print()
+    asterisk_tree(height, level + 1)
 
-def asterisk_tree(height,level):
-  if level > height:
-    return
-  for j in range(2*level-1):
-    print("*", end="")
-  print()
-  asterisk_tree(height, level + 1)
-
-n=int(input("Enter height of tree : "))
+n = int(input("Enter height of tree: "))
 asterisk_tree(n, 1)
+
